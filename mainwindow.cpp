02f9f72ee7ba5db1c2b1ce9c21d570de65088e9b
sonichy <sonichy@163.com>
2017-08-26 13:13:15 +0800

    修改：     QQMusic 	修改：     mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -103,8 +103,10 @@ MainWindow::MainWindow(QWidget *parent) :
     plt.setColor(QPalette::WindowText, color);
     DesktopLyric->ui->label_lyric->setPalette(plt);
     QString sfont = readSettings(QDir::currentPath() + "/config.ini", "Font");
-    QStringList SLFont = sfont.split(",");
-    DesktopLyric->ui->label_lyric->setFont(QFont(SLFont.at(0),SLFont.at(1).toInt(),SLFont.at(2).toInt(),SLFont.at(3).toInt()));
+    if(sfont!=""){
+        QStringList SLFont = sfont.split(",");
+        DesktopLyric->ui->label_lyric->setFont(QFont(SLFont.at(0),SLFont.at(1).toInt(),SLFont.at(2).toInt(),SLFont.at(3).toInt()));
+    }
     connect(DesktopLyric,SIGNAL(pushButton_lyric_toggle()),ui->pushButton_lyric,SLOT(toggle()));
     DesktopLyric->show();
 
