50731b356831de389af8a43774654abac62ed09d
sonichy <sonichy@163.com>
2020-09-09 19:57:51 +0800

    增加desktop图标

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-s="[Desktop Entry]\nName=QQ音乐\nComment=Media player\nExec=`pwd`/QQMusic\nIcon=`pwd`/icon.png\nPath=`pwd`\nTerminal=false\nType=Application\nCategories=AudioVideo;"
+s="[Desktop Entry]\nName=QQ音乐\nComment=Media player\nExec=`pwd`/QQMusic\nIcon=`pwd`/QQMusic.png\nPath=`pwd`\nTerminal=false\nType=Application\nCategories=AudioVideo;"
 echo -e $s > QQMusic.desktop
 cp `pwd`/QQMusic.desktop ~/.local/share/applications/QQMusic.desktop
